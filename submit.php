dfe82ee3c0be108bf44f989ad9af7653da449f57
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2023-10-12 11:48:24 +0100

    submit.php modified

--- submit.php
+++ submit.php
@@ -28,7 +28,14 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $result = $conn->query($sql);
 
         if ($result->num_rows > 0) {
-            echo '<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" href="./style.css" /><meta charset="UTF-8">    <meta name="viewport" content="width=device-width, initial-scale=1.0">    <title>Welcome to our competition</title>    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" /></head><body><div class="container"><div class="card"><div class="card-body"><h5 class="card-title">Error!</h5><p class="card-text">This email address has already been used.</p></div></div></div></body></html>';
+            echo '<!DOCTYPE html><html lang="en">
+            <head>
+                <link rel="stylesheet" href="./style.css" />
+                <meta charset="UTF-8">    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Welcome to our competition</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+            </head>
+            <body><div class="container"><div class="card"><div class="card-body"><h5 class="card-title">Error!</h5><p class="card-text">This email address has already been used.</p></div></div></div></body></html>';
             exit();
         }
 
@@ -36,7 +43,24 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $sql = "INSERT INTO user_details (user_name, email, birthday) VALUES ('$user_name', '$email', '$birthday')";
         
         if ($conn->query($sql) === TRUE) {
-            echo '<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" href="./style.css" /><meta charset="UTF-8">    <meta name="viewport" content="width=device-width, initial-scale=1.0">    <title>Welcome to our competition</title>    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" /></head><body><div class="container"><div class="card"><div class="card-body"><h5 class="card-title">Thank you for entering our competition!</h5><p class="card-text">The lucky winner will be drawn at random after the competition closes. Good luck!</p></div></div></div></body></html>';
+            echo '<!DOCTYPE html><html lang="en">
+                    <head>
+                       <link rel="stylesheet" href="./style.css" /><meta charset="UTF-8">
+                       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <title>Welcome to our competition</title>
+                       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+                    </head>
+                                    <body>
+                                        <div class="container">
+                                            
+                                                <div class="card-body">
+                                                    <h5 class="card-title">Thank you for entering our competition!</h5>
+                                                    <p class="card-text">The lucky winner will be drawn at random after the competition closes. Good luck!</p>
+                                                </div> 
+                                                <a class="btn btn-primary" href="./index.html" role="button">Go back</a>
+                                        </div>
+                                    </body>
+                 </html>';
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
